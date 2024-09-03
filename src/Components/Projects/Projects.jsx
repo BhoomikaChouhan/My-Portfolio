f9494dba4b4318a9da4d17198a79d809d2959fd9
Bhoomika Chouhan <104447847+BhoomikaChouhan@users.noreply.github.com>
2024-09-03 19:06:16 +0530

    Update Projects.jsx

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './Projects.css'
-import {Swiper, SwiperSlide} from 'swiper/react'
+import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/swiper.min.css';
 import Sidebar from "../../img/sidebar.png"
 import Hotel from "../../img/hotel.PNG"
@@ -8,29 +8,27 @@ import Codetranslator from "../../img/code trans.PNG"
 
 export const Projects = () => {
   return (
-    <div className="projects" id="Projects" style={{marginLeft: "3rem"}}>
-        {/* heading */}
-        <span>Recent Projects</span>
-        <span>Portfolio</span>
+    <div className="projects" id="Projects" style={{ marginLeft: "3rem" }}>
+      {/* heading */}
+      <span>Recent Projects</span>
+      <span>Portfolio</span>
 
-        {/* slider */}
-        <Swiper
+      {/* slider */}
+      <Swiper
         spaceBetween={10}
         slidesPerView={2}
         grabCursor={true}
-        className="projects-slider" 
-        sp       
-        >
-            <SwiperSlide
-            >
-               <a href="https://SyntaxSwitch.vercel.app/"> <img src={Codetranslator} alt="" /></a></SwiperSlide>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={Hotel} alt="" />
-            </SwiperSlide>
-            
-        </Swiper>
-
+        className="projects-slider"
+      >
+        <SwiperSlide>
+          <a href="https://SyntaxSwitch.vercel.app/" target="_blank" rel="noopener noreferrer">
+            <img src={Codetranslator} alt="Code Translator" />
+          </a>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Hotel} alt="Hotel" />
+        </SwiperSlide>
+      </Swiper>
     </div>
   )
 }
